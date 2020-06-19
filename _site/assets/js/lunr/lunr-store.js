b0e992045b8bd9a0eff0ba743264a0ed760e49c0
Raymond Sumargo <raymond.sumargo@kumparan.com>
2020-06-19 20:51:34 +0700

    Change 1

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Powerful GraphQL Automated Test With Karate",
-        "excerpt":"What is GraphQL Most of us probably already know about REST, as REST seems to be the go-to standard for APIs. Some of us may be thinking, what GraphQL is? How is it different from REST? Well I was thinking the same when I first interviewed at kumparan. I had...","categories": ["karate","automated test"],
+        "excerpt":"What is GraphQL Most of us probably already know about REST, as REST seems to be the go-to standard for APIs. Some of us may be thinking, what is a GraphQL? How is it different from REST? Well I was thinking the same when I first interviewed at kumparan. I...","categories": ["karate","automated test"],
         "tags": ["karate","automated test"],
         "url": "http://localhost:4000/karate/automated%20test/karate-graphql/",
         "teaser": null
